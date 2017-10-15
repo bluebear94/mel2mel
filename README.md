f847b49c93c416d92822289da226d8f4d12a38e9
bluebear94 <uruwi@protonmail.com>
2017-10-15 08:25:36 -0400

    mel

---
+++ README.md
@@ -0,0 +1,7 @@
+## mel2mel
+
+It's memey. 'Nuff said.
+
+### Licence
+
+Just do whatever you want with this.
---
+++ mel.js
@@ -0,0 +1,47 @@
+console.log("bepis");
+
+var exID = chrome.i18n.getMessage("@@extension_id");
+var melURL = "chrome-extension://" + exID + "/images/mel.png";
+var mel = document.createElement("img");
+mel.setAttribute("src", melURL);
+
+function getFontSizeOfParent(node) {
+  var s = getComputedStyle(node.parentNode, null)["font-size"];
+  return parseInt(s);
+}
+
+function getMelNodeFor(node) {
+  var size = getFontSizeOfParent(node);
+  var mymel = mel.cloneNode(true);
+  mymel.setAttribute("height", (size) + "px");
+  mymel.setAttribute("width", (45.0 * size / 38.0) + "px");
+  return mymel;
+}
+
+function touchNode(node) {
+  var tagName = node.tagName;
+  if (tagName == "SCRIPT" || tagName == "LINK") return;
+  var children = node.childNodes;
+  for (var child of children) {
+    if (child.nodeType == Document.TEXT_NODE) {
+      var oldText = child.textContent;
+      var separators = oldText.split(/mel/ig);
+      var slen = separators.length;
+      var mymel = getMelNodeFor(child);
+      for (var i = 0; i < slen - 1; ++i) {
+        child.before(separators[i]);
+        child.before(mymel);
+      }
+      child.replaceWith(separators[slen - 1]);
+    } else touchNode(child);
+  }
+}
+
+/*
+document.addEventListener("DOMContentLoaded", function(event) { 
+  touchNode(window.document);
+  console.log("bepis2");
+});
+*/
+
+touchNode(window.document);
